87016b9788626bcf8086bbc044fa9082345f2cb9
SWETA_RAJ <swetaraj@google.com>
2025-02-03 18:57:15 +0530

    updated examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,7 @@
+# ---------------------------------------------------------------------------------------
+# Example 1 : Non-PCI GCS Bucket with internal encryption (CMEK handled by module)
+# ---------------------------------------------------------------------------------------
+
 module "non-pci-gcs" {
   source             = "../terraform-gcp-gcs"
   bucket_name_prefix = "gcs-non-pci-321"
@@ -5,10 +9,7 @@ module "non-pci-gcs" {
   regions            = ["northamerica-northeast1", "northamerica-northeast2"]
   environment        = "prod"
   versioning         = true
-  soft_delete_policy = {
-    retention_duration_seconds = 900000
-  }
-  bucket_type = "non-pci"
+  bucket_type        = "non-pci"
   internal_encryption_config = {
     create_encryption_key = true
     prevent_destroy       = false
@@ -25,7 +26,11 @@ module "non-pci-gcs" {
   ]
 }
 
-module "non-pci-gcs_cmek" {
+# ---------------------------------------------------------------------------------
+# Example 2 : Non-PCI GCS Bucket with encryption key passed by the customer
+# ---------------------------------------------------------------------------------
+
+module "non-pci-gcs-cmek" {
   source             = "../terraform-gcp-gcs"
   bucket_name_prefix = "gcs-non-pci-321-cmek"
   project_id         = "pr-hvpc-1056d88565a4"
@@ -33,15 +38,22 @@ module "non-pci-gcs_cmek" {
   regions            = ["northamerica-northeast1", "northamerica-northeast2"]
   environment        = "non-prod"
   versioning         = true
-  soft_delete_policy = {
-    retention_duration_seconds = 900000
-  }
-  bucket_type = "non-pci"
+  bucket_type        = "non-pci"
   kms_key_names = {
     "northamerica-northeast1" = "projects/pr-hvpc-1056d88565a4/locations/northamerica-northeast1/keyRings/pci-ring/cryptoKeys/pci-key-mon"
     "northamerica-northeast2" = "projects/pr-hvpc-1056d88565a4/locations/northamerica-northeast2/keyRings/pci-ring-tor/cryptoKeys/pci-key-tor"
   }
+  iam_members = [
+    {
+      role   = "roles/storage.objectUser"
+      member = "serviceAccount:b-32-988@pr-hvpc-1056d88565a4.iam.gserviceaccount.com"
+    }
+  ]
 }
+
+# ---------------------------------------------------------------------------------------
+# Example 3 : PCI GCS Bucket with internal encryption (CMEK handled by module)
+# ---------------------------------------------------------------------------------------
 
 module "pci-gcs" {
   source             = "../terraform-gcp-gcs"
@@ -50,15 +62,22 @@ module "pci-gcs" {
   regions            = ["northamerica-northeast1", "northamerica-northeast2"]
   environment        = "prod"
   versioning         = true
-  soft_delete_policy = {
-    retention_duration_seconds = 900000
-  }
-  bucket_type = "pci"
+  bucket_type        = "pci"
   internal_encryption_config = {
     create_encryption_key = true
     prevent_destroy       = false
   }
+  iam_members = [
+    {
+      role   = "roles/storage.objectUser"
+      member = "serviceAccount:b-32-988@pr-hvpc-1056d88565a4.iam.gserviceaccount.com"
+    }
+  ]
 }
+
+# ---------------------------------------------------------------------------------
+# Example 4 : PCI GCS Bucket with encryption key passed by the customer
+# ---------------------------------------------------------------------------------
 
 module "pci-gcs-cmek" {
   source             = "../terraform-gcp-gcs"
@@ -68,31 +87,15 @@ module "pci-gcs-cmek" {
   regions            = ["northamerica-northeast1", "northamerica-northeast2"]
   environment        = "prod"
   versioning         = true
-  soft_delete_policy = {
-    retention_duration_seconds = 900000
-  }
-  bucket_type = "pci"
+  bucket_type        = "pci"
   kms_key_names = {
     "northamerica-northeast1" = "projects/pr-hvpc-1056d88565a4/locations/northamerica-northeast1/keyRings/pci-ring/cryptoKeys/pci-key-mon"
     "northamerica-northeast2" = "projects/pr-hvpc-1056d88565a4/locations/northamerica-northeast2/keyRings/pci-ring-tor/cryptoKeys/pci-key-tor"
   }
+  iam_members = [
+    {
+      role   = "roles/storage.objectUser"
+      member = "serviceAccount:b-32-988@pr-hvpc-1056d88565a4.iam.gserviceaccount.com"
+    }
+  ]
 }
-
-
-module "pci-gcs-test-depends_on" {
-  source             = "../terraform-gcp-gcs"
-  bucket_name_prefix = "gcs-pci-321567"
-  project_id         = "pr-hvpc-1056d88565a4"
-  regions            = ["northamerica-northeast1", "northamerica-northeast2"]
-  environment        = "prod"
-  versioning         = true
-  soft_delete_policy = {
-    retention_duration_seconds = 900000
-  }
-  bucket_type = "pci"
-  internal_encryption_config = {
-    create_encryption_key = true
-    prevent_destroy       = false
-  }
-}
-
