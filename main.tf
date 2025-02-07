aca2a8c06ff9123958fc1b85244df4997f2b448b
SWETA_RAJ <swetaraj@google.com>
2025-02-07 10:18:29 +0530

    updated requirements

--- main.tf
+++ main.tf
@@ -19,17 +19,16 @@
 # -----------------------------------------------------------
 
 module "non_pci_gcs_buckets" {
-  count              = var.bucket_type == "non-pci" ? 1 : 0
-  source             = "./modules/gcs-buckets-non-pci"
-  project_id         = var.project_id
-  project_number     = var.project_number
-  bucket_name_prefix = var.bucket_name_prefix
-  regions            = var.regions
-  versioning         = var.versioning
-  labels             = var.labels
-  storage_class      = var.storage_class
-  autoclass          = var.autoclass
-  # environment                = var.environment
+  count                      = var.bucket_type == "non-pci" ? 1 : 0
+  source                     = "./modules/gcs-buckets-non-pci"
+  project_id                 = var.project_id
+  project_number             = var.project_number
+  bucket_name_prefix         = var.bucket_name_prefix
+  regions                    = var.regions
+  versioning                 = var.versioning
+  labels                     = var.labels
+  storage_class              = var.storage_class
+  autoclass                  = var.autoclass
   iam_members                = var.iam_members
   retention_policy           = var.retention_policy
   lifecycle_rules            = var.lifecycle_rules
@@ -44,17 +43,16 @@ module "non_pci_gcs_buckets" {
 #  PCI Bucket (Lifecycle rule and retention period is set to 7 years)
 # ---------------------------------------------------------------------
 module "pci_gcs_buckets" {
-  count              = var.bucket_type == "pci" ? 1 : 0
-  source             = "./modules/gcs-buckets-pci"
-  project_id         = var.project_id
-  project_number     = var.project_number
-  bucket_name_prefix = var.bucket_name_prefix
-  regions            = var.regions
-  versioning         = var.versioning
-  labels             = var.labels
-  storage_class      = var.storage_class
-  autoclass          = var.autoclass
-  # environment                = var.environment
+  count                      = var.bucket_type == "pci" ? 1 : 0
+  source                     = "./modules/gcs-buckets-pci"
+  project_id                 = var.project_id
+  project_number             = var.project_number
+  bucket_name_prefix         = var.bucket_name_prefix
+  regions                    = var.regions
+  versioning                 = var.versioning
+  labels                     = var.labels
+  storage_class              = var.storage_class
+  autoclass                  = var.autoclass
   iam_members                = var.iam_members
   force_destroy              = var.force_destroy
   public_access_prevention   = "enforced"
